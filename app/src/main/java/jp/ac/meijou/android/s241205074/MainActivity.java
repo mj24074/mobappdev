cf27b9eb7ea4aee8ef33f046a8ef1fc66c349202
Yoshitaka Kurokawa <185391787+mj24074@users.noreply.github.com>
2025-09-02 13:30:23 +0900

    テキスト編集の演習

--- app/src/main/java/jp/ac/meijou/android/s241205074/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205074/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.ac.meijou.android.s241205074;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205074.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+        binding.button.setOnClickListener((view) -> {
+            var text = binding.editTextText.getText().toString();
+            binding.text.setText(text);
+        });
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.text.setText(s.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int i2) {
+
+
+            }
+        });
+
+        }
 }
